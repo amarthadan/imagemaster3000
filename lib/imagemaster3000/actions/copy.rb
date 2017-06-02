ffbd8db56afb755bd8045f742b755224b00b971d
Michal Kimle <kimle.michal@gmail.com>
2017-06-02 10:57:36 +0200

    Update 'copy' action to support file renaming

--- lib/imagemaster3000/actions/copy.rb
+++ lib/imagemaster3000/actions/copy.rb
@@ -1,25 +1,44 @@
 module Imagemaster3000
   module Actions
     class Copy
-      attr_accessor :source, :target
+      attr_accessor :filename, :source, :target, :name
 
-      def initialize(source: nil, target: nil)
+      def initialize(source: nil, target: nil, name: nil)
         raise Imagemaster3000::Errors::ArgumentError, 'neither source nor target can be nil' if source.empty? || target.empty?
 
-        @source = source
-        @target = File.join(Imagemaster3000::Settings[:'definitions-dir'], target)
+        @filename = source
+        @source = File.join(Imagemaster3000::Settings[:'definitions-dir'], 'files', source)
+        @target = target
+        @name = name
         logger.debug "Created action #{inspect}"
       end
 
       def run(image_file)
         logger.debug "Running 'copy' action with source #{source.inspect} and target #{target.inspect} on file #{image_file.inspect}"
+        copy image_file
+        rename image_file if name
+      rescue Imagemaster3000::Errors::CommandExecutionError => ex
+        raise Imagemaster3000::Errors::ActionError, ex
+      end
+
+      private
+
+      def copy(image_file)
         Imagemaster3000::Utils::CommandExecutioner.execute Imagemaster3000::Settings[:'binaries-virt-copy-in'],
                                                            '-a',
                                                            image_file,
                                                            source,
                                                            target
-      rescue Imagemaster3000::Errors::CommandExecutionError => ex
-        raise Imagemaster3000::Errors::ActionError, ex
+      end
+
+      def rename(image_file)
+        Imagemaster3000::Utils::CommandExecutioner.execute Imagemaster3000::Settings[:'binaries-guestfish'],
+                                                           '-a',
+                                                           image_file,
+                                                           '-i',
+                                                           'mv',
+                                                           File.join(target, filename),
+                                                           File.join(target, name)
       end
     end
   end
