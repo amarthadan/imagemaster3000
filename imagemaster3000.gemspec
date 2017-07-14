ff665e8250e9a0025ffc9bf4a7fae13c8a90901d
Mamut <janca@ics.muni.cz>
2017-07-14 13:06:56 +0200

    UPDATE: loading definitions from git repository

--- imagemaster3000.gemspec
+++ imagemaster3000.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'bundler', '~> 1.15'
+  spec.add_development_dependency 'git', '~> 1.3'
   spec.add_development_dependency 'rake', '~> 12.0'
   spec.add_development_dependency 'rspec', '~> 3.5'
   spec.add_development_dependency 'rubocop', '~> 0.48'
--- lib/imagemaster3000/main_process.rb
+++ lib/imagemaster3000/main_process.rb
@@ -1,25 +1,42 @@
 module Imagemaster3000
   class MainProcess
-    def run
-      logger.debug 'Loading images from definitions'
-      images = Imagemaster3000::Definitions::Parser.parse_image_definitions
-      logger.debug 'Processing images'
-      images.each { |image| process_image image }
+    attr_accessor :definitions
+    attr_accessor :images
 
-      logger.debug 'Generating image list'
-      image_list = Imagemaster3000::ImageList::Signer.sign(Imagemaster3000::ImageList::Generator.generate(images))
-      File.write Imagemaster3000::Settings[:'image-list'], image_list
+    def run
+      process_definitions
+      process_images
+      generate_image_list
       Imagemaster3000::Cleaner.clean
     ensure
       Imagemaster3000::Cleaner.write_clean_file images.map(&:local_filename)
+      definitions.clean
     end
 
     private
 
-    def process_image(image)
-      image.download
-      image.verify! if image.respond_to? :verify!
-      image.actions.each { |action| action.run image.local_filename } unless image.actions.blank?
+    def process_definitions
+      logger.debug "Download definitions from #{Imagemaster3000::Settings['definitions-git-repo'].inspect}"
+      @definitions = Imagemaster3000::Definitions::Downloader.new Imagemaster3000::Settings['definitions-git-repo']
+      Imagemaster3000::Settings['definitions-dir'] = definitions.path
+      logger.debug 'Loading images from definitions'
+      @images = Imagemaster3000::Definitions::Parser.parse_image_definitions
+    end
+
+    def process_images
+      logger.debug 'Processing images'
+      images.each do |image|
+        logger.debug "Processing image #{image.name.inspect}"
+        image.download
+        image.verify! if image.respond_to? :verify!
+        image.actions.each { |action| action.run image.local_filename } unless image.actions.blank?
+      end
+    end
+
+    def generate_image_list
+      logger.debug 'Generating image list'
+      image_list = Imagemaster3000::ImageList::Signer.sign(Imagemaster3000::ImageList::Generator.generate(images))
+      File.write Imagemaster3000::Settings[:'image-list'], image_list
     end
   end
 end
